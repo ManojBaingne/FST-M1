0f247ba3a4b7bf37a2d3f5f6f8145bb8b56e9314
ManojBaingne <manoj.nie2015@gmail.com>
2025-02-24 11:11:07 +0530

    Add files via upload

---
+++ Java/Activities/CustomException.java
@@ -0,0 +1,28 @@
+package activity;
+
+public class CustomException extends Exception{
+	
+	private String message = null;
+
+	CustomException(String message ){
+		this.message=message;
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;		
+	}
+	
+	static void ExceptionTest(String str) throws CustomException {
+		if(str==null)
+		{
+			throw new CustomException("String value is null");
+		}
+		
+		else {
+			
+			System.out.println(str);
+		}
+	}
+
+}
